6b30492341ed848ebcae56f5e0ac25f50b308da0
eduoop <dudugoiabal@hotmail.com>
2022-01-24 12:04:42 -0300

    let && const

--- public/bundle.js
+++ public/bundle.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var n1 = 'Eduardo';
+var n2 = 'Pedro';
+n2 = 'João'; // True
+// n1 = 'Mateus' // False
+
+console.log(n1);
+console.log(n2);
---
+++ src/main.js
@@ -0,0 +1,11 @@
+const n1 = 'Eduardo'
+
+let n2 = 'Pedro'
+
+n2 = 'João' // True
+// n1 = 'Mateus' // False
+
+console.log(n1)
+console.log(n2)
+
+
