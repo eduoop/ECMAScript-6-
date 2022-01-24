53ce3ffdba1bb7b4c6ee66948c7083e1ed78b303
eduoop <dudugoiabal@hotmail.com>
2022-01-24 12:54:46 -0300

    other functions

--- public/bundle.js
+++ public/bundle.js
@@ -1,9 +1,47 @@
 "use strict";
 
-var n1 = 'Eduardo';
-var n2 = 'Pedro';
-n2 = 'João'; // True
-// n1 = 'Mateus' // False
+//  ---------------------- map() && forEach()  ----------------------
+var numbs = [1, 2, 3, 4, 5];
+numbs.forEach(function (item, index) {
+  console.log("O item \xE9 ".concat(item, ", de index ").concat(index));
+});
+var newNumbs = numbs.map(function (item) {
+  return item + 1;
+});
+console.log(newNumbs); //  ---------------------- reduce() ----------------------
 
-console.log(n1);
-console.log(n2);
+var numbers = [1, 2, 3, 4, 5];
+var total = numbers.reduce(function (total, next) {
+  return total + next;
+});
+console.log(total); //  ---------------------- filter() && find() ----------------------
+
+var todos = [{
+  text: 'Lavar a pia',
+  done: false
+}, {
+  text: 'Estender a roupa',
+  done: true
+}, {
+  text: 'Passar a roupa',
+  done: true
+}];
+var doneTodos = todos.filter(function (todo) {
+  return todo.done;
+});
+console.log(doneTodos);
+var findTodo = todos.find(function (todo) {
+  return todo.text === 'Passar a roupa';
+});
+console.log(findTodo); //  ---------------------- findIndex() ----------------------
+
+var index = todos.findIndex(function (todo) {
+  return todo.text === 'Passar a roupa';
+});
+console.log(index); //  ---------------------- some() ----------------------
+
+var cars = ['fusca', 'civic', 'hb20'];
+var hasFusca = cars.some(function (car) {
+  return car === 'fusca';
+});
+console.log(hasFusca);
--- src/main.js
+++ src/main.js
@@ -1,11 +1,62 @@
-const n1 = 'Eduardo'
+//  ---------------------- map() && forEach()  ----------------------
 
-let n2 = 'Pedro'
+const numbs = [1, 2, 3, 4, 5]
 
-n2 = 'João' // True
-// n1 = 'Mateus' // False
+numbs.forEach((item, index) => {
+    console.log(`O item é ${item}, de index ${index}`)
+})
 
-console.log(n1)
-console.log(n2)
+const newNumbs = numbs.map((item) => {
+    return item + 1
+})
+
+console.log(newNumbs)
+//  ---------------------- reduce() ----------------------
+
+const numbers = [1, 2, 3, 4, 5]
+
+const total = numbers.reduce((total, next) => {
+    return total + next
+})
+
+console.log(total)
+
+
+//  ---------------------- filter() && find() ----------------------
+const todos = [
+    { text: 'Lavar a pia', done: false },
+    { text: 'Estender a roupa', done: true },
+    { text: 'Passar a roupa', done: true }
+]
+
+const doneTodos = todos.filter((todo) => {
+    return todo.done
+})
+
+console.log(doneTodos)
+
+const findTodo = todos.find((todo) => {
+    return todo.text === 'Passar a roupa'
+})
+
+console.log(findTodo)
+
+//  ---------------------- findIndex() ----------------------
+
+const index = todos.findIndex((todo) => {
+    return todo.text === 'Passar a roupa'
+})
+
+console.log(index)
+
+//  ---------------------- some() ----------------------
+
+const cars = ['fusca', 'civic', 'hb20']
+
+const hasFusca = cars.some((car) => {
+    return car === 'fusca'
+})
+
+console.log(hasFusca)
 
 
